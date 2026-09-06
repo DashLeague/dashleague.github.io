05f739b9a15fd55027b2aeff68a4352af1589f92
codi fredericks <68131899+codi-fredericks@users.noreply.github.com>
2026-09-06 14:45:31 +1200

    add more routes for pages

--- site/src/router/index.ts
+++ site/src/router/index.ts
@@ -1,6 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from "../views/HomeView.vue"
 import MatchView from "../views/MatchesView.vue"
+import MatchPageView from "../views/MatchPageView.vue"
+import PlayerPageView from "../views/PlayerPageView.vue"
+import TeamPageView from "../views/TeamPageView.vue"
+import Error404View from "../views/Error404View.vue"
 
 const routes = [
   {
@@ -12,6 +16,30 @@ const routes = [
     path: "/matches",
     name: "matches",
     component: MatchView
+  },
+  {
+    path: "/match/:matchID",
+    name: "match page",
+    component: MatchPageView,
+    props: true
+  },
+  {
+    path: "/team/:teamID",
+    name: "team page",
+    component: TeamPageView,
+    props: true
+  },
+  {
+    path: "/player/:playerID",
+    name: "player page",
+    component: PlayerPageView,
+    props: true
+  },
+
+  {
+    path: '/:pathMatch(.*)*',
+    name: "error 404",
+    component: Error404View
   }
 ]
 
