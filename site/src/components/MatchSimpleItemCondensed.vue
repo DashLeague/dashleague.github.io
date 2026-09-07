a11d0521c5698387abadfcc987978b14183c7e24
codi fredericks <68131899+codi-fredericks@users.noreply.github.com>
2026-09-07 20:32:19 +1200

    rename MatchSimpleItem to be consistent

--- site/src/components/MatchSimpleItemCondensed.vue
+++ site/src/components/MatchSimpleItemCondensed.vue
@@ -1,7 +1,7 @@
 <!-- This component is the same as MatchSimpleItem but has no images -->
 <template>
   <div class="match-simple-item">
-      <h3>DATE</h3>
+      <h3>{{ match_time }}</h3>
       <h3>{{ team_home_name }}</h3>
       <p>{{ team_home_score }}</p>
       <h3>{{ team_away_name }}</h3>
@@ -10,12 +10,16 @@
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
   team_home_id: String,
   team_home_name: String,
   team_home_score: Number,
   team_away_id: String,
   team_away_name: String,
   team_away_score: Number,
+  match_timestamp: Number
 })
+
+// *1000 to take it from the miliseconds timestamps js and ts normally use to seconds
+const match_time = new Date(props.match_timestamp*1000).toLocaleString();
 </script>
